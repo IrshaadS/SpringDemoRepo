012262eec5b51e72f4da0ff5f71ec22545823e1c
IrshaadS <Irshaad_S@rediffmail.com>
2019-01-02 15:30:23 +0530

    Default Profile Demo

--- src/main/java/com/springguru/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/springguru/didemo/services/PrimaryGermanGreetingService.java
@@ -3,17 +3,18 @@
  */
 package com.springguru.didemo.services;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service!";
+        return "German Greeting Service";
     }
 }
